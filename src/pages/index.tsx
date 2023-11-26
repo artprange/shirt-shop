7b19737d24d63d81e114c7f6c2f4d5daa3dcdfec
artprange <arthur.prange@gmail.com>
2023-11-26 19:13:54 -0300

    setting up main home elements

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,32 @@
-import { styled } from "@/pages/styles/index"
 
-const Button = styled('button', {
-  backgroundColor: '$rocketseat',
-  borderRadius:4,
-  border:0,
-  padding: '4px 8px',
 
-  
+import { HomeContainer, Product } from "./styles/pages/home"
+import camiseta1 from "../pages/assets/1.png"
+import camiseta2 from "../pages/assets/2.png"
+import camiseta3 from "../pages/assets/3.png"
+import Image from "next/image"
 
-})
 
 export default function Home() {
   return (
-    <Button> Enviar</Button>
+    <HomeContainer>
+      <Product>
+        <Image  src={camiseta1} alt="" width={520} height={480}/>
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 69,90</span>
+        </footer>
+      </Product> 
+
+        <Product>
+        <Image  src={camiseta2} alt="" width={520} height={480}/>
+
+        <footer>
+          <strong>Camiseta Y</strong>
+          <span>R$ 69,90</span>
+        </footer>
+      </Product>  
+      
+    </HomeContainer>
 )}
