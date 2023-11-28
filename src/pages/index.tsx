bd0ad998ae091700d4653c618dd20a490e49cd40
artprange <arthur.prange@gmail.com>
2023-11-27 21:32:02 -0300

    setting server side props

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,7 +11,7 @@ import Image from "next/image"
 
 
 
-export default function Home() {
+export default function Home(props: any) {
   const [sliderRef] = useKeenSlider({
     slides:{
       perView: 3,
@@ -21,6 +21,9 @@ export default function Home() {
 
   return (
     <HomeContainer ref={sliderRef} className="keen-slider" >
+      <pre>{JSON.stringify(props.list)}</pre>
+
+
       <Product className="keen-slider__slide" >
         <Image  src={camiseta1} alt="" width={520} height={480}/>
 
@@ -49,3 +52,13 @@ export default function Home() {
       
     </HomeContainer>
 )}
+
+
+export const getServerSideProps = async() =>{
+  await new Promise(resolve =>setTimeout(resolve, 2000))
+  return{
+    props:{
+      list:[1, 2, 3]
+    }
+  }
+}
