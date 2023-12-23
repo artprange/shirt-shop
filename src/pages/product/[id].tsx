ae038bda5390eb639e2b149f800f3bf6c4c692c0
artprange <arthur.prange@gmail.com>
2023-12-23 17:56:21 -0300

    checkout route implemented

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -16,10 +16,15 @@ interface ProductProps {
       imageUrl: string;
       price: string;
       description: string;
+      defaultPriceId: string;
     }
 }
 
 export default function Product({ product }: ProductProps) {
+
+  function handleBuyProduct(){
+    console.log(product.defaultPriceId)
+  }
 
 
  
@@ -35,7 +40,7 @@ export default function Product({ product }: ProductProps) {
 
               <p>{product.description}</p>
 
-              <button>Comprar Agora</button>
+              <button onClick={handleBuyProduct}>Comprar Agora</button>
           </ProductDetails>
 
       </ProductContainer>
@@ -89,6 +94,7 @@ export const getStaticProps: GetStaticProps<any, { id: string }> = async ({ para
           currency: 'BRL',
         }).format(price.unit_amount / 100),
         description: product.description || '',
+        defaultPriceId: price.id,
       },
     },
     revalidate: 60 * 60 * 1,
