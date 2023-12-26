98882af6d1290439fa689f47779476147a4639dd
artprange <arthur.prange@gmail.com>
2023-12-26 16:00:06 -0300

    styles all ok!

--- src/styles/pages/success.ts
+++ src/styles/pages/success.ts
@@ -9,7 +9,37 @@ export const SuccessContainer = styled('main', {
     justifyContent: 'center',
     margin: '0 auto',
     height: 656,
-});
+
+    h1:{
+        fontSize: '$2xl',
+        color: '$gray-100',
+    },
+    p:{
+        fontSize: '$xl',
+        color: '$gray-300',
+        maxWidth: 560,
+        textAlign: 'center',
+        marginTop: '2rem',
+        lineHeight: '1.4rem',
+
+    
+    },
+
+    a: {
+        display: 'block',
+        marginTop: '5rem',
+        fontSize: '$lg',
+        color: '$green500',
+        textDecoration: 'none',
+        fontWeight: 'bold',
+
+        '&hover':{
+            color: '$green300',
+    }
+     },
+    } 
+
+);
 
 export const ImageContainer = styled('main', {
     width: '100%',
@@ -18,6 +48,7 @@ export const ImageContainer = styled('main', {
     background: 'linear-gradient(180deg, #1ea483 0%, #7465d4 100%)',
     borderRadius: 8,
     padding: '0.25rem',
+    marginTop:'4rem',
     display: 'flex',
     alignItems: 'center',
     justifyContent: 'center',
